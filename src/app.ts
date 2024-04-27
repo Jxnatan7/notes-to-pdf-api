c90370d6ddde5e18b17838e743f9a54290c34d47
Jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-04-27 14:57:14 -0300

    :cyclone: refactor: Muda o formato da resposta da API para base64.

--- src/app.ts
+++ src/app.ts
@@ -12,17 +12,24 @@ app.get("/note-to-pdf", async (req, res) => {
         }
 
         const browser = await puppeteer.launch();
+
         const page = await browser.newPage();
 
         await page.goto(url, { waitUntil: "networkidle0" });
+
+        const searchResultSelector = '#heading2';
+
+        await page.waitForSelector(searchResultSelector);
+
+        await page.click(searchResultSelector);
 
         const pdfBuffer = await page.pdf({ format: "A4" });
 
         await browser.close();
 
-        res.contentType("application/pdf");
-        res.status(200);
-        res.send(pdfBuffer);
+        const base64String = pdfBuffer.toString('base64');
+
+        res.status(200).json({ pdfBase64: base64String });
     } catch (error) {
         console.error("Error generating PDF:", error);
         res.status(500).send("An error occurred while generating the PDF.");
